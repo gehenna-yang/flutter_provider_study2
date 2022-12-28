e0d1b2ac8b908bda78f9f63bb64124382f5ebd3c
gehenna-yang <winer0707@naver.com>
2022-12-28 17:26:43 +0900

    provider logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpodtest/riverpod/provider_observer.dart';
 import 'package:riverpodtest/screen/home_screen.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [Logger()],
       child: MaterialApp(
         home: HomeScreen(),
       ),
